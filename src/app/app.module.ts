1445d7e61af74b49444c0888fb5222dde57d3f27
Sudarshan101 <sudarshan.vis101@gmail.com.com>
2018-05-16 14:37:39 +0530

    insta share in ionic 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { SocialSharing } from '@ionic-native/social-sharing';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { Camera } from '@ionic-native/camera';
 @NgModule({
   declarations: [
     MyApp,
@@ -24,6 +24,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    SocialSharing,
+    Camera,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { SocialSharing } from '@ionic-native/social-sharing';
+import { Camera } from '@ionic-native/camera';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+	currentImage = null;
+  constructor(public navCtrl: NavController, public socialSharing: SocialSharing, private camera: Camera) {
 
   }
-
+  loadImage() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY      
+    }
+ 
+    this.camera.getPicture(options).then(data => {
+      this.currentImage = 'data:image/jpeg;base64,' + data;
+     }, err => {
+      // Handle error
+      console.log(err)
+     });
+  }
+  share(){
+		this.socialSharing.shareViaInstagram('Share instagram', this.currentImage).then(() => {
+		  // Success!
+		}).catch(() => {
+		  // Error!
+		});
+  }
 }
